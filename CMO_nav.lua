e870a9e590a96c25ce0d4365a6f759b42ce632a7
Olaf Seisler <olaf.seisler@gmail.com>
2022-01-18 01:35:25 +0200

    Add event creation to single script (WIP)

--- CMO_nav.lua
+++ CMO_nav.lua
@@ -1,70 +1,80 @@
-local radius = 6371e3
-
-local salvoSize = 2
+local salvoSize = 5
 local interval = 5
 local v_missile = 500
-
-local missiles = 0
-local flip
+local wpn_dbid = 377
+local mount_dbid = 387
 
 local event
-local trig
-local con
-local act
 
-local function BuildCircleOfRPs(theside, unit, range)
+function BuildCircleOfRPs(theside, attacker, range) --Builds detection area
   local refPoints = {}
-  local tableofcirclepoints = World_GetCircleFromPoint({latitude=unit.latitude,longitude=unit.longitude,numpoints=4,radius=range})
+  local tableofcirclepoints = World_GetCircleFromPoint({latitude=attacker.latitude,longitude=attacker.longitude,numpoints=4,radius=range})
   local counter = 1
   for k,v in ipairs(tableofcirclepoints) do
-    table.insert(refPoints, ScenEdit_AddReferencePoint({side=theside,latitude=v.latitude,longitude=v.longitude, relativeto=unit.guid, name=unit.name .. "-" .. counter}).name)
+    table.insert(refPoints, ScenEdit_AddReferencePoint({side=theside,latitude=v.latitude,longitude=v.longitude, relativeto=attacker.guid, name=attacker.name .. "-" .. counter}).name)
     counter = counter + 1
   end
   return refPoints
 end
 
-local unit = ScenEdit_GetUnit({name='SSGN 726 Ohio [DDS]', guid='GWKW9V-0HM9I5EHB3JF3'})
-local circlePoints = BuildCircleOfRPs('BLUFOR', unit, 1)
+local side_name = ScenEdit_PlayerSide()
+local attacker_id = {name='SSGN 726 Ohio [DDS]', guid='RZD6HZ-0HMEPM7A970LA'}
+local target_contact_id = {name='67N6 Gamma-D', guid='RZD6HZ-0HMEPM7A971IQ'}
+local attacker = ScenEdit_GetUnit(attacker_id)
+local target = VP_GetUnit(target_contact_id)
+local circlePoints = BuildCircleOfRPs(side_name, attacker, 0.25)
 
-function calculateDistance(a, deltaT)
-    return (v_missile^2*deltaT^2 + 2*a*v_missile*deltaT) / (2*(a + v_missile*deltaT))
+local scriptText = "local id_a = {name='SSGN 726 Ohio [DDS]', guid='RZD6HZ-0HMEPM7A970LA'}\r\n"..
+"local id_b = {name='67N6 Gamma-D', guid='RZD6HZ-0HMEPM7A971IQ'}\r\n"..
+'local unit_a = ScenEdit_GetUnit(id_a)\r\n'..
+'local unit_b = VP_GetUnit({guid=id_b.guid})\r\n'..
+'\r\n'..
+'local salvo_size = 5\r\n'..
+'local interval = 5\r\n'..
+'local v_missile = 500\r\n'..
+'\r\n'..
+'function CalculateDistance(a, deltaT)\r\n'..
+'    local dist_m = (v_missile^2*deltaT^2 + 2*a*v_missile*deltaT) / (2*(a + v_missile*deltaT))\r\n'..
+'    return dist_m / 1852\r\n'..
+'end\r\n'..
+'\r\n'..
+'local get_value = ScenEdit_GetKeyValue(unit_a.guid)\r\n'..
+'local missiles\r\n'..
+'if get_value ~= "" then\r\n'..
+'    missiles = tonumber(get_value)\r\n'..
+'else\r\n'..
+'    missiles = 1\r\n'..
+'end\r\n'..
+'\r\n'..
+'local perp_dist = CalculateDistance(Tool_Range(unit_a.guid, unit_b.guid), interval*(salvo_size-missiles))\r\n'..
+'local perp_bearing = Tool_Bearing(unit_a.guid, unit_b.guid) + 90 * (-1)^missiles\r\n'..
+'local perp_point = World_GetPointFromBearing({latitude=unit_a.latitude, longitude=unit_a.longitude, distance=perp_dist, bearing=perp_bearing})\r\n'..
+'\r\n'..
+'local msl = ScenEdit_UnitX()\r\n'..
+'msl.course = {{lat=perp_point.latitude, lon=perp_point.longitude}, {lat=msl.course[#msl.course].latitude, lon=msl.course[#msl.course].longitude}}\r\n'..
+'\r\n'..
+'if missiles >= salvo_size then\r\n'..
+'    ScenEdit_ClearKeyValue(unit_a.guid)\r\n'..
+'else\r\n'..
+'    ScenEdit_SetKeyValue(unit_a.guid, tostring(missiles + 1))\r\n'..
+'end'
+
+local status, exception = pcall(ScenEdit_GetEvent, "ToT CM Strike")
+if status then
+    ScenEdit_MsgBox(tostring(exception), 1)
+else
+    event = ScenEdit_SetEvent("ToT CM Strike", {mode="add", IsActive=true, IsRepeatable=true})
+
+    ScenEdit_SetTrigger({mode='add',
+            type='UnitEntersArea',
+            name='MissileSpawned', 
+            targetfilter={TargetType='6'},
+            area=circlePoints})
+    
+    ScenEdit_SetAction({mode="add", name="SetMslPath", type="LuaScript", ScriptText = scriptText})
+
+    ScenEdit_SetEventTrigger('ToT CM Strike', {mode='add', name='MissileSpawned'})
+    ScenEdit_SetEventAction('ToT CM Strike', {mode='add', name='SetMslPath'})
 end
 
-if trig == nil then
-    local trig = ScenEdit_SetTrigger({mode='add',
-    type='UnitEntersArea',
-    name='MissileSpawned', 
-    targetfilter={TargetType='6'},
-    area=circlePoints})
-end
-
-if act == nil then  
-    local script = 'print("works")'
-    act = ScenEdit_SetAction({mode='add',type='Points',name='MyFunction',
-    SideID="BLUFOR", PointChange=10})
-    print(missiles)
-end
-
-if event == nil then
-    local event = ScenEdit_SetEvent('event', {mode='add'})
-    ScenEdit_SetEventTrigger('event', {mode='add', name='MissileSpawned'})
-    ScenEdit_SetEventAction('event', {mode='add', name='MyFunction'})
-    event.isRepeatable = true
-end
-
-local scriptText = 'local salvoSize = 2 \r\n'..
-'local interval = 5 \r\n'..
-'local v_missile = 500 \r\n'..
-'local  missiles = 0 \r\n'..
-'local unit = ScenEdit_GetUnit({name="SSGN 726 Ohio [DDS]", guid="GWKW9V-0HM9I5EHB3JF3"}) \r\n'..
-'function calculateDistance(a, deltaT) \r\n'..
-'    return (v_missile^2*deltaT^2 + 2*a*v_missile*deltaT) / (2*(a + v_missile*deltaT)) \r\n'..
-'end \r\n'..
-'local msl = ScenEdit_UnitX() \r\n'..
-'local missiles = missiles + 1 \r\n'..
-'local perp_dist = calculateDistance(Tool_Range("unit.guid", "target.guid"), interval*(salvoSize-missiles)) \r\n'..
-'local perp_point = World_GetPointFromBearing({latitude="unit.latitude", longitude="unit.longitude", distance = perp_dist, bearing = Tool_Bearing("unit.guid", "target.guid")}) \r\n'..
-'local course = {} \r\n'..
-'table.insert(course, perp_point) \r\n'..
-'msl.course = course'
-  
+ScenEdit_AttackContact(attacker.guid, target_contact_id.guid, {mode='1', mount=mount_dbid, weapon=wpn_dbid, qty=5})
--- tot_msl_strike.lua
+++ tot_msl_strike.lua
@@ -2,19 +2,17 @@ local id_a = {name='SSGN 726 Ohio [DDS]', guid='RZD6HZ-0HMEPM7A970LA'}
 local id_b = {name='67N6 Gamma-D', guid='RZD6HZ-0HMEPM7A971IQ'}
 local unit_a = ScenEdit_GetUnit(id_a)
 local unit_b = VP_GetUnit({guid=id_b.guid})
-local side_name = ScenEdit_PlayerSide()
 
-local salvo_size = 3
+local salvo_size = 5
 local interval = 5
-local v_missile = 500 
+local v_missile = 500
 
 function CalculateDistance(a, deltaT)
     local dist_m = (v_missile^2*deltaT^2 + 2*a*v_missile*deltaT) / (2*(a + v_missile*deltaT))
     return dist_m / 1852
-end 
+end
 
 local get_value = ScenEdit_GetKeyValue(unit_a.guid)
-
 local missiles
 if get_value ~= "" then
     missiles = tonumber(get_value)
@@ -22,13 +20,12 @@ else
     missiles = 1
 end
 
-
 local perp_dist = CalculateDistance(Tool_Range(unit_a.guid, unit_b.guid), interval*(salvo_size-missiles))
 local perp_bearing = Tool_Bearing(unit_a.guid, unit_b.guid) + 90 * (-1)^missiles
 local perp_point = World_GetPointFromBearing({latitude=unit_a.latitude, longitude=unit_a.longitude, distance=perp_dist, bearing=perp_bearing})
---print(perp_dist)
 
-ScenEdit_AddReferencePoint({side=side_name, latitude=perp_point.latitude, longitude=perp_point.longitude, name=unit_a.name.."-"..missiles})
+local msl = ScenEdit_UnitX()
+msl.course = {{lat=perp_point.latitude, lon=perp_point.longitude}, {lat=msl.course[#msl.course].latitude, lon=msl.course[#msl.course].longitude}}
 
 if missiles >= salvo_size then
     ScenEdit_ClearKeyValue(unit_a.guid)
