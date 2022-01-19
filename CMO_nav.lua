51b1dfcc02424805511b8b912fb768ceeaa37ffc
Olaf Seisler <olaf.seisler@gmail.com>
2022-01-20 01:56:27 +0200

    Finished basic event system setup

--- CMO_nav.lua
+++ CMO_nav.lua
@@ -1,9 +1,14 @@
-local salvo_size = 5
-local interval = 5
-local v_missile = 500
-local wpn_dbid = 377
-local mount_dbid = 2533
-local event
+-- [REQUIRED]
+local salvo_size = 5 -- How many missiles to fire
+local interval = 5 -- How much time between shots
+local v_missile = 500 -- Missile speed in knots
+local r_missile = 1600 -- Missile max range
+local wpn_dbid = 377 -- Missile database ID
+local mount_dbid = 387 -- Mount database ID. Find it from unit Weapons>Add Mount search
+local side_name = ScenEdit_PlayerSide() -- Currently viewed from side
+local attacker_id = {name='SSGN 726 Ohio [DDS]', guid='RZD6HZ-0HMEPM7A970LA'} -- ID of the firing unit. Replace with your desired unit ID.
+local target_contact_id = {name='67N6 Gamma-D', guid='RZD6HZ-0HMEPM7A971IQ'} -- ID of the enemy CONTACT. Get it from Unit Orders>Scenario Editor
+-- [REQUIRED]
 
 function BuildCircleOfRPs(theside, attacker, range) --Builds detection area
   local refPoints = {}
@@ -16,28 +21,30 @@ function BuildCircleOfRPs(theside, attacker, range) --Builds detection area
   return refPoints
 end
 
-local side_name = ScenEdit_PlayerSide()
-local attacker_id = {name='SSGN 726 Ohio [DDS]', guid='GWKW9V-0HM9I5EHB3JF3'}
-local target_contact_id = {name='67N6 Gamma-D', guid='GWKW9V-0HM9I5EHB3JDT'}
+function UnixToDotnet(unix) -- Converts Unix time to DotNet ticks because Command be like that
+  return unix * 10000000 + 621355968000000000
+end
+
 local attacker = ScenEdit_GetUnit(attacker_id)
-local target = VP_GetUnit(target_contact_id)
+local target = VP_GetUnit(target_contact_id) -- Resolved unit
 local circlePoints = BuildCircleOfRPs(side_name, attacker, 0.25)
 
-local scriptText = "local id_a = {name='SSGN 726 Ohio [DDS]', guid='GWKW9V-0HM9I5EHB3JF3'}\r\n"..
-"local id_b = {name='67N6 Gamma-D', guid='GWKW9V-0HM9I5EHB3JDT'}\r\n"..
-'local unit_a = ScenEdit_GetUnit(id_a)\r\n'..
-'local unit_b = VP_GetUnit({guid=id_b.guid})\r\n'..
+local scriptText = string.format('local id_a = "%s"\r\n'..
+'local id_b = "%s"\r\n'..
+'local unit_a = ScenEdit_GetUnit({guid=id_a})\r\n'..
+'local unit_b = VP_GetUnit({guid=VP_GetContact({guid=id_b}).guid})\r\n'..
 '\r\n'..
-'local salvo_size = 5\r\n'..
-'local interval = 5\r\n'..
-'local v_missile = 500\r\n'..
+'local salvo_size = %d\r\n'..
+'local interval = %d\r\n'..
+'local v_missile = %d\r\n'..
+'local r_missile = %d\r\n'..
 '\r\n'..
 'function CalculateDistance(a, deltaT)\r\n'..
 '    local dist_m = (v_missile^2*deltaT^2 + 2*a*v_missile*deltaT) / (2*(a + v_missile*deltaT))\r\n'..
 '    return dist_m / 1852\r\n'..
 'end\r\n'..
 '\r\n'..
-'local get_value = ScenEdit_GetKeyValue(unit_a.guid)\r\n'..
+'local get_value = ScenEdit_GetKeyValue("Missile")\r\n'..
 'local missiles\r\n'..
 'if get_value ~= "" then\r\n'..
 '    missiles = tonumber(get_value)\r\n'..
@@ -45,17 +52,20 @@ local scriptText = "local id_a = {name='SSGN 726 Ohio [DDS]', guid='GWKW9V-0HM9I
 '    missiles = 1\r\n'..
 'end\r\n'..
 '\r\n'..
-'local perp_dist = CalculateDistance(Tool_Range(unit_a.guid, unit_b.guid), interval*(salvo_size-missiles))\r\n'..
-'local total_dist = perp_dist + math.sqrt(perp_dist^2 + Tool_Range(unit_a.guid, unit_b.guid)^2)\r\n'..
-'if total_dist > r_missile then perp_dist = r_missile end\r\n'..
-'local perp_bearing = Tool_Bearing(unit_a.guid, unit_b.guid) + 90 * (-1)^missiles\r\n'..
+'local perp_dist = CalculateDistance(Tool_Range(id_a, id_b), interval*(salvo_size-missiles))\r\n'..
+'local total_dist = perp_dist + math.sqrt(perp_dist^2 + Tool_Range(id_a, id_b)^2)\r\n'..
+'if total_dist > r_missile then\r\n'..
+'  perp_dist = r_missile\r\n'..
+'  ScenEdit_MsgBox("Target too far for simultaneous impact!", 1)\r\n'..
+'end\r\n'..
+'local perp_bearing = Tool_Bearing(id_a, id_b) + 90 * (-1)^missiles\r\n'..
 'local perp_point = World_GetPointFromBearing({latitude=unit_a.latitude, longitude=unit_a.longitude, distance=perp_dist, bearing=perp_bearing})\r\n'..
 '\r\n'..
 'local msl = ScenEdit_UnitX()\r\n'..
 'msl.course = {{lat=perp_point.latitude, lon=perp_point.longitude}, {lat=msl.course[#msl.course].latitude, lon=msl.course[#msl.course].longitude}}\r\n'..
 '\r\n'..
 'if missiles >= salvo_size then\r\n'..
-'    ScenEdit_ClearKeyValue(unit_a.guid)\r\n'..
+'    ScenEdit_ClearKeyValue("Missile")\r\n'..
 '    ScenEdit_SetEvent("ToT CM Strike", {mode="remove", IsActive=true, IsRepeatable=true})\r\n'..
 '    ScenEdit_SetAction({mode="remove", type="LuaScript", name="SetMslPath"})\r\n'..
 '    ScenEdit_SetTrigger({mode="remove", type="UnitEntersArea", name="MslSpawned"})\r\n'..
@@ -63,30 +73,32 @@ local scriptText = "local id_a = {name='SSGN 726 Ohio [DDS]', guid='GWKW9V-0HM9I
 '         ScenEdit_DeleteReferencePoint({side=unit_a.side, name=unit_a.name.."-"..i})\r\n'..
 '    end\r\n'..
 'else\r\n'..
-'    ScenEdit_SetKeyValue(unit_a.guid, tostring(missiles + 1))\r\n'..
-'end\r\n'
+'    ScenEdit_SetKeyValue("Missile", tostring(missiles + 1))\r\n'..
+'end\r\n', attacker.guid, target_contact_id.guid, salvo_size, interval, v_missile, r_missile)
 
-local status, exception = pcall(ScenEdit_GetEvent, "ToT CM Strike")
+local status, exception = pcall(ScenEdit_GetEvent, "ToT CM Strike") -- Sets up a firing event if there isn't one ongoing
 if status then
     ScenEdit_MsgBox(tostring(exception), 1)
 else
-    event = ScenEdit_SetEvent("ToT CM Strike", {mode="add", IsActive=true, IsRepeatable=true})
+    ScenEdit_SetEvent("ToT CM Strike", {mode="add", IsActive=true, IsRepeatable=true})
 
     ScenEdit_SetTrigger({mode='add',
             type='UnitEntersArea',
-            name='MslSpawned', 
+            name='MslSpawned',
             targetfilter={TargetType='6', TargetSubType='2001', SpecificUnitClass=nil, SpecificUnit=nil, TargetSide=side_name},
-            area=circlePoints})
-    
+            area=circlePoints, -- Filter for spawned guided weapons
+            ETOA=UnixToDotnet(ScenEdit_CurrentTime()), -- Set up event time cause it might break otherwise
+            LTOA=UnixToDotnet(ScenEdit_CurrentTime() + 86400)})
+
     ScenEdit_SetAction({mode="add", name="SetMslPath", type="LuaScript", ScriptText = scriptText})
 
     ScenEdit_SetEventTrigger('ToT CM Strike', {mode='add', name='MslSpawned'})
     ScenEdit_SetEventAction('ToT CM Strike', {mode='add', name='SetMslPath'})
 end
-
+-- Attack target
 local attack = ScenEdit_AttackContact(attacker.guid, target_contact_id.guid, {mode='1', mount=mount_dbid, weapon=wpn_dbid, qty=salvo_size})
 if attack then
-print("Attack in progress")
+  print("Attack in progress")
 else
-print("Attack Unsuccessful")
+  print("Attack unsuccessful")
 end
--- tot_msl_strike.lua
+++ tot_msl_strike.lua
@@ -6,14 +6,14 @@ local unit_b = VP_GetUnit({guid=id_b.guid})
 local salvo_size = 5
 local interval = 5
 local v_missile = 500
-local r_missile = 1600 
+local r_missile = 1600
 
-function CalculateDistance(a, deltaT)
+function CalculateDistance(a, deltaT) -- Calculates tangential path length to make everything arrive simultaneously
     local dist_m = (v_missile^2*deltaT^2 + 2*a*v_missile*deltaT) / (2*(a + v_missile*deltaT))
     return dist_m / 1852
 end
 
-local get_value = ScenEdit_GetKeyValue(unit_a.guid)
+local get_value = ScenEdit_GetKeyValue("Missile") -- Tries to get value from permanent storage
 local missiles
 if get_value ~= "" then
     missiles = tonumber(get_value)
@@ -23,7 +23,10 @@ end
 
 local perp_dist = CalculateDistance(Tool_Range(unit_a.guid, unit_b.guid), interval*(salvo_size-missiles))
 
-if perp_dist > r_missile then perp_dist = r_missile end
+if perp_dist > r_missile then
+    perp_dist = r_missile
+    ScenEdit_MsgBox("Target too far for simultaneous impact!", 1)
+end
 
 local perp_bearing = Tool_Bearing(unit_a.guid, unit_b.guid) + 90 * (-1)^missiles
 local perp_point = World_GetPointFromBearing({latitude=unit_a.latitude, longitude=unit_a.longitude, distance=perp_dist, bearing=perp_bearing})
@@ -32,7 +35,7 @@ local msl = ScenEdit_UnitX()
 msl.course = {{lat=perp_point.latitude, lon=perp_point.longitude}, {lat=msl.course[#msl.course].latitude, lon=msl.course[#msl.course].longitude}}
 
 if missiles >= salvo_size then
-    ScenEdit_ClearKeyValue(unit_a.guid)
+    ScenEdit_ClearKeyValue("Missile") -- Clears all storage and events if salvo finished
     ScenEdit_SetEvent("ToT CM Strike", {mode="remove", IsActive=true, IsRepeatable=true})
     ScenEdit_SetAction({mode="remove", type="LuaScript", name="SetMslPath"})
     ScenEdit_SetTrigger({mode="remove", type="UnitEntersArea", name="MslSpawned"})
@@ -40,6 +43,6 @@ if missiles >= salvo_size then
         ScenEdit_DeleteReferencePoint({side=unit_a.name, name=unit_a.name.."-"..i})
     end
 else
-    ScenEdit_SetKeyValue(unit_a.guid, tostring(missiles + 1))
+    ScenEdit_SetKeyValue("Missile", tostring(missiles + 1)) -- Saves value to permanent storage
 end
 
